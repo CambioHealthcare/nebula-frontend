55fff5e6f5b7ac6b190a79e890dc6528eae1a91f
cambiowaltersson <joakim.waltersson@cambio.se>
2016-03-29 14:55:09 +0200

    Check node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,22 @@
 (function() {
     "use strict"
     
+    var semver = require("semver");
+    
     desc("Default task");
-    task("default", function() {
+    task("default", [ "version" ], function() {
         console.log("\n\nBuild OK");
     });
-        
-    
+    desc("Check Node version");
+    task("version", function() {
+       console.log("Checking Node version");
+       
+       var packageJson = require("./package.json");
+       var expectedVersion = packageJson.engines.node;
+       var actualVersion = process.version;
+       
+       if (!semver.gte(actualVersion, expectedVersion)) {
+           fail("Wrong version, expected " + expectedVersion + " got " + actualVersion);
+       }
+    });
 }());
