847018f49630c8d58c42803f7719edf7133f80e7
cambiowaltersson <joakim.waltersson@cambio.se>
2016-03-30 08:33:34 +0200

    Automatic linting using JSHint at build time

--- Jakefile.js
+++ Jakefile.js
@@ -1,16 +1,16 @@
 (function() {
-    "use strict"
+    "use strict";
     
     var semver = require("semver");
+    var jshint = require("simplebuild-jshint");
     
     desc("Default task");
-    task("default", [ "version" ], function() {
+    task("default", [ "version", "lint" ], function() {
         console.log("\n\nBuild OK");
     });
     desc("Check Node version");
     task("version", function() {
-       console.log("Checking Node version");
-       
+       console.log("Checking Node version: .");
        var packageJson = require("./package.json");
        var expectedVersion = packageJson.engines.node;
        var actualVersion = process.version;
@@ -19,4 +19,28 @@
            fail("Wrong version, expected " + expectedVersion + " got " + actualVersion);
        }
     });
+
+    task("lint", function() {
+        process.stdout.write("Linting JavaScript: ");
+        jshint.checkFiles({
+            files: "Jakefile.js",
+            options: {
+                bitwise: true,
+                eqeqeq: true,
+                forin: true,
+                freeze: true,
+                futurehostile: true,
+                latedef: "nofunc",
+                noarg: true,
+                nocomma: true,
+                nonbsp: true,
+                nonew: true,
+                strict: true,
+                undef: true,
+                node: true,
+                browser: true
+            },
+            globals: {}
+        }, complete, fail);
+    }, { async: true });
 }());
