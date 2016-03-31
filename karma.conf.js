b972e22a95c2735da5c56cb7b047bff487590ad7
cambiowaltersson <joakim.waltersson@cambio.se>
2016-03-31 15:23:42 +0200

    Enable karma-commonjs plugin to support "require" in browser tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'commonjs'],
 
 
     // list of files / patterns to load in the browser
@@ -24,6 +24,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/*.js': ['commonjs']
     },
 
 
